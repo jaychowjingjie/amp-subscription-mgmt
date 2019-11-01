732868c07a96afcaaef5b0b111cc55953f4869aa
Santhosh Bomma <v-sanbom@microsoft.com>
2019-10-31 22:36:15 -0700

    replaced string literals with constants file

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import jwt
 from cryptography.x509 import load_pem_x509_certificate
 from cryptography.hazmat.backends import default_backend
 import re
+import constant
 
 app = Flask(__name__)
 app.config.from_object(app_config)
@@ -66,14 +67,14 @@ def landingpage():
     if not _user_is_authenticated():
         return redirect(url_for("login"))
     token = request.args.get('token')
-    subscription = get_subscription_by_token(token)
-    found_sub_id = hasattr(subscription, 'id')
+    subscription = get_subscriptionid_by_token(token)
     if not token or 'id' not in subscription:
-        return render_template('error.html', user=session["user"])  
+        return render_template(constant.ERROR_PAGE, user=session["user"])  
     subscription_data = get_subscription(subscription['id'])
     plans = get_availableplans(subscription['id'])
     
-    return render_template('managesubscription.html', user=session["user"], subscription = subscription_data, available_plans= plans)
+    return render_template(constant.MANAGE_SUBSCRIPTION_PAGE, user=session["user"], subscription = subscription_data, available_plans= plans)
+
 
 @app.route("/edit/<subscriptionid>")
 def edit(subscriptionid):
@@ -81,7 +82,7 @@ def edit(subscriptionid):
         return redirect(url_for("login"))
     subscription = get_subscription(subscriptionid)
     plans = get_availableplans(subscriptionid)
-    return render_template('managesubscription.html', user=session["user"], subscription = subscription, available_plans= plans)
+    return render_template(constant.MANAGE_SUBSCRIPTION_PAGE, user=session["user"], subscription = subscription, available_plans= plans)
 
 @app.route("/update", methods=['POST'])
 def updatesubscription():
@@ -96,12 +97,12 @@ def updatesubscription():
         selected_plan = request.form['selectedplan']
         response = update_subscriptionplan(selected_subscription, selected_plan)
     else:
-        return redirect(url_for("error.html"))
+        return redirect(url_for(constant.ERROR_PAGE))
 
     if response.status_code == 202 or response.status_code:
         return redirect(url_for("login"))
     else:
-        return render_template('error.html', user=session["user"], response_statuscode = response.status_code)
+        return render_template(constant.ERROR_PAGE, user=session["user"], response_statuscode = response.status_code)
     
 @app.route("/operations/<subscriptionid>")
 def operations(subscriptionid):
@@ -109,7 +110,7 @@ def operations(subscriptionid):
     if not _user_is_authenticated():
         return redirect(url_for("login"))
     sub_operations = get_sub_operations(subscriptionid)
-    return render_template('suboperations.html', user=session["user"], operations = sub_operations)
+    return render_template(constant.SUBSCRIPTION_OPERATIONS_PAGE, user=session["user"], operations = sub_operations)
 
 @app.route("/updateoperation/<operationid>")
 def updateoperation(operationid):
@@ -166,56 +167,6 @@ def _user_is_authenticated():
     if not session.get("user") or session.get("user") is None or not token or token is None:
         return False
     return True
-
-def get_subscriptions():
-    subscriptions_data=  call_marketplace_api(app_config.MARKETPLACEAPI_ENDPOINT+ app_config.MARKETPLACEAPI_API_VERSION)
-    return subscriptions_data
- 
-def get_subscription(subscription_id):
-    subscription_data=  call_marketplace_api(  # Use token to call downstream service
-        app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription_id + app_config.MARKETPLACEAPI_API_VERSION)
-    return subscription_data
-
-def get_subscription_by_token(token):
-    subscription_data=  call_marketplace_api(app_config.MARKETPLACEAPI_ENDPOINT +"/resolve" + app_config.MARKETPLACEAPI_API_VERSION
-                                            ,request_method='POST', resolve_token= token, resolve_url= True)
-    return subscription_data.json()
-
-def get_availableplans(subscription):
-    availableplans = call_marketplace_api(
-        request_url=app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription + "/listAvailablePlans" + app_config.MARKETPLACEAPI_API_VERSION)
-    return availableplans
-
-def activate_subscriptionplan(subscription, plan_id):
-    request_plan_payload = "{\"planId\": \""+ plan_id +"\" ,\"quantity\": \"\" }"
-    activateresponse = call_marketplace_api(app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription + "/activate"+  app_config.MARKETPLACEAPI_API_VERSION,                                      
-    'POST', 
-    request_plan_payload
-    )
-    return activateresponse
-
-def update_subscriptionplan(subscription, plan_id):
-    request_plan_payload = "{\"planId\": \""+ plan_id +"\" }"
-    updateresponse = call_marketplace_api(app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription + app_config.MARKETPLACEAPI_API_VERSION,                                      
-    'PATCH', 
-    request_plan_payload
-    )
-    
-    updateresponseheaders = updateresponse.headers
-    updateresponsestatuscode = updateresponse.status_code
-    if 'Operation-Location' in updateresponseheaders and updateresponsestatuscode ==202:
-        operation_location = updateresponseheaders['Operation-Location']
-        subscription_id = re.search('subscriptions/(.*)/operations', operation_location).group(1)
-        operation_id = re.search('operations/(.*)\?api-version', operation_location).group(1)
-        operation_datetime = updateresponseheaders['Date']
-        # store in table storage
-        if subscription_id and operation_id:
-            #connect to table storgae
-            request_payload ={ 'PartitionKey':subscription_id ,'RowKey':operation_id, 'updatedtime' : operation_datetime, 'SubscriptionId':subscription_id ,'OperationId':operation_id }
-            store_in_azure_table(app_config.ISV_OPS_STORAGE_TABLE_NAME, request_payload)
-        else:
-            return redirect(url_for('error.html'), user=session["user"])
-    return updateresponse
 
 def validate_jwt_token(access_token):
     try:
@@ -296,10 +247,61 @@ def store_in_azure_table(table_name, request_payload):
         # Insert the entity into the table
         table_service.insert_entity(table_name, request_payload)
 
+
+def get_subscriptions():
+    subscriptions_data=  call_marketplace_api(constant.GET_SUBSCRIPTIONS_ENDPOINT)
+    return subscriptions_data
+ 
+def get_subscription(subscription_id):
+    subscription_data=  call_marketplace_api(constant.GET_SUBSCRIPTION_ENDPOINT(subscription_id))
+    return subscription_data
+
+def get_subscriptionid_by_token(token):
+    subscription_data=  call_marketplace_api(constant.RESOLVE_ENDPOINT, request_method='POST', resolve_token= token, resolve_url= True)
+    return subscription_data.json()
+
+def get_availableplans(subscription):
+    availableplans = call_marketplace_api(request_url=constant.GET_SUBSCRIPTION_PLANS(subscription))
+    return availableplans
+
+def activate_subscriptionplan(subscription, plan_id):
+    request_plan_payload = "{\"planId\": \""+ plan_id +"\" ,\"quantity\": \"\" }"
+    activateresponse = call_marketplace_api(constant.ACTIVATE_SUBSCRIPTION_ENDPOINT(subscription),
+    'POST', request_plan_payload)
+    return activateresponse
+
+def update_subscriptionplan(subscription, plan_id):
+    request_plan_payload = "{\"planId\": \""+ plan_id +"\" }"
+    updateresponse = call_marketplace_api(constant.UPDATE_SUBSCRIPTION_ENDPOINT(subscription), 'PATCH', request_plan_payload)
+    
+    updateresponseheaders = updateresponse.headers
+    updateresponsestatuscode = updateresponse.status_code
+    if 'Operation-Location' in updateresponseheaders and updateresponsestatuscode ==202:
+        operation_location = updateresponseheaders['Operation-Location']
+        subscription_id = re.search('subscriptions/(.*)/operations', operation_location).group(1)
+        operation_id = re.search('operations/(.*)\?api-version', operation_location).group(1)
+        operation_datetime = updateresponseheaders['Date']
+        # store in table storage
+        if subscription_id and operation_id:
+            #connect to table storgae
+            request_payload ={ 'PartitionKey':subscription_id ,'RowKey':operation_id, 'updatedtime' : operation_datetime, 'SubscriptionId':subscription_id ,'OperationId':operation_id, 'ToPlan': plan_id }
+            store_in_azure_table(app_config.ISV_OPS_STORAGE_TABLE_NAME, request_payload)
+        else:
+            return redirect(url_for(constant.ERROR_PAGE), user=session["user"])
+    return updateresponse
+
 def get_sub_operations(subscription):
-    sub_operations_data =  call_marketplace_api(  # Use token to call downstream service
-        app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription + "/operations" + app_config.MARKETPLACEAPI_API_VERSION)
+    sub_operations_data =  call_marketplace_api(constant.GET_SUBSCRIPTION_OPERATIONS_ENDPOINT(subscription))
     return sub_operations_data
+
+def get_sub_operation(subscription, operation):
+    sub_operation_data =  call_marketplace_api(constant.GET_UPDATE_SUBSCRIPTION_OPERATION_ENDPOINT(subscription, operation))
+    return sub_operation_data
+
+def update_sub_operation(subscription, operation):
+    update_operation_response =  call_marketplace_api(constant.GET_UPDATE_SUBSCRIPTION_OPERATION_ENDPOINT(subscription, operation),
+    'PATCH', op_payload)
+    return update_operation_response
 
 def get_marketplace_access_token():
     token_url = app_config.AUTHORITY + app_config.MARKETPLACEAPI_TENANTID + '/oauth2/token'
---
+++ constant.py
@@ -0,0 +1,26 @@
+import app_config
+
+MANAGE_SUBSCRIPTION_PAGE = 'managesubscription.html'
+SUBSCRIPTION_OPERATIONS_PAGE = 'suboperations.html'
+ERROR_PAGE = 'error.html'
+
+RESOLVE_ENDPOINT =app_config.MARKETPLACEAPI_ENDPOINT +"/resolve" + app_config.MARKETPLACEAPI_API_VERSION
+def ACTIVATE_SUBSCRIPTION_ENDPOINT(subscription_id):
+    return app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription_id + "/activate"+  app_config.MARKETPLACEAPI_API_VERSION
+
+GET_SUBSCRIPTIONS_ENDPOINT =app_config.MARKETPLACEAPI_ENDPOINT + app_config.MARKETPLACEAPI_API_VERSION
+
+def GET_SUBSCRIPTION_ENDPOINT(subscription_id):
+    return app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription_id + app_config.MARKETPLACEAPI_API_VERSION
+
+def GET_SUBSCRIPTION_PLANS(subscription_id):
+    return app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription_id + "/listAvailablePlans" + app_config.MARKETPLACEAPI_API_VERSION
+
+def UPDATE_SUBSCRIPTION_ENDPOINT(subscription_id):
+    return app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription_id + app_config.MARKETPLACEAPI_API_VERSION
+
+def GET_SUBSCRIPTION_OPERATIONS_ENDPOINT(subscription_id):
+    return app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription_id + "/operations" + app_config.MARKETPLACEAPI_API_VERSION
+
+def GET_UPDATE_SUBSCRIPTION_OPERATION_ENDPOINT(subscription_id, operation_id):
+    return app_config.MARKETPLACEAPI_ENDPOINT +"/"+ subscription_id + "/operations/" + operation_id + app_config.MARKETPLACEAPI_API_VERSION
